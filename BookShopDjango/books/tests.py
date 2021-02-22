6fabf68ceb48a651254a8537e653ec5b9da7c801
Ivan Holikov <laurence4312@gmail.com>
2021-02-22 22:54:01 +0200

    Add Book TestCase

--- BookShopDjango/books/tests.py
+++ BookShopDjango/books/tests.py
@@ -13,3 +13,22 @@ class PublisherModelTest(TestCase):
         publisher = Publisher.objects.get(id=1)
         expected_object_name = f'{publisher.publisher}'
         self.assertEquals(expected_object_name, 'Some Test publisher')
+
+
+class BookModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        publisher = Publisher.objects.create(publisher='Some Test publisher')
+        Book.objects.create(title='Some Test Book',
+                            author='',
+                            description='',
+                            publish_year=2021,
+                            stock=3,
+                            price=120,
+                            publisher=publisher)
+
+    def test_title_content(self):
+        publisher = Book.objects.get(id=1)
+        expected_object_name = f'{publisher.publisher}'
+        self.assertEquals(expected_object_name, 'Some Test publisher')
