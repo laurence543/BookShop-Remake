6b50f02e12a4605afef492ae257ef124f42c75e1
Ivan Holikov <laurence4312@gmail.com>
2021-02-22 18:47:52 +0200

    Add TestCase for PublisherModel

--- BookShopDjango/books/tests.py
+++ BookShopDjango/books/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Book, Publisher
+
 
 # Create your tests here.
+class PublisherModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Publisher.objects.create(publisher='Some Test publisher')
+
+    def test_publisher_content(self):
+        publisher = Publisher.objects.get(id=1)
+        expected_object_name = f'{publisher.publisher}'
+        self.assertEquals(expected_object_name, 'Some Test publisher')
