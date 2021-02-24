1317e2227d00b61a06e70444f222dfc375ef4f4c
Ivan Holikov <laurence4312@gmail.com>
2021-02-24 18:23:41 +0200

    Replaced some item's content for view

--- frontend/src/components/Book.js
+++ frontend/src/components/Book.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import { List, Avatar, Space } from 'antd';
 import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
-
-const listData = [];
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://ant.design',
-    title: `ant design part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description:
-      'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
+import './Book.css';
 
 const IconText = ({ type, text }) => (
     <span>
@@ -30,9 +18,9 @@ const Book = (props) => {
                 onChange: page => {
                     console.log(page);
                 },
-                pageSize: 3,
+                pageSize: 10,
             }}
-            dataSource={listData}
+            dataSource={props.data}
             footer={
                 <div>
                     <b>ant design</b> footer part
@@ -42,9 +30,9 @@ const Book = (props) => {
                 <List.Item
                     key={item.title}
                     actions={[
-                        <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
-                        <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
-                        <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
+                        //<IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
+                        //<IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
+                        //<IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
                     ]}
                     extra={
                         <img
@@ -55,11 +43,15 @@ const Book = (props) => {
                     }
                 >
                     <List.Item.Meta
-                        avatar={<Avatar src={item.avatar} />}
                         title={<a href={item.href}>{item.title}</a>}
-                        description={item.description}
+                        //description={item.description}
+
                     />
                     {item.content}
+                    <div className="author-div">{item.author}</div>
+                    <div>{item.description}</div>
+
+
                 </List.Item>
             )}
         />
