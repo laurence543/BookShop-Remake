636f8970c476acd100f0b523b7cade465dd5f350
Ivan Holikov <laurence4312@gmail.com>
2021-02-24 23:57:38 +0200

    Added Essential code for BookDetailView.js

---
+++ frontend/src/containers/BookDetailView.js
@@ -0,0 +1,30 @@
+import React from "react";
+import axios from "axios";
+import Books from "../components/Book";
+import { Card } from "antd";
+
+class BookDetail extends React.Component {
+  state = {
+    book: {}
+  };
+
+  componentDidMount() {
+    const bookID = this.props.match.params.bookID;
+    axios.get("http://127.0.0.1:8000/books/api/${bookID}").then(res => {
+      this.setState({
+        book: res.data
+      });
+      console.log(res.data);
+    });
+  }
+
+  render() {
+    return (
+      <Card title={this.state.book.title}>
+        <p>{this.state.book.description}</p>
+      </Card>
+    );
+  }
+}
+
+export default BookDetail;
