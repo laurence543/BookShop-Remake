33247066b98fa71efb17368cf56bb8003e978e8b
Ivan Holikov <laurence4312@gmail.com>
2021-02-25 15:37:11 +0200

    Made working routes for books, :booksID (GET)

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,1 +1,13 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+import BookList from "./containers/BookListView";
+import BookDetail from "./containers/BookDetailView";
+
+const BaseRouter = () => (
+    <div>
+        <Route exact path="/books" component={BookList}/>
+        <Route exact path="/books/:bookID/" component={BookDetail}/>
+    </div>
+);
+
+export default BaseRouter;
