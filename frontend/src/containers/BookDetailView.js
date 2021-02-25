34abc4780b99f6d0312fd8045157a6d18ad0ea30
Ivan Holikov <laurence4312@gmail.com>
2021-02-25 15:34:31 +0200

    Made first working ver BookDetailView.js

--- frontend/src/containers/BookDetailView.js
+++ frontend/src/containers/BookDetailView.js
@@ -1,30 +1,30 @@
 import React from "react";
 import axios from "axios";
 import Book from "../components/Book";
-import { Card } from "antd";
+import {Card} from "antd";
 
 class BookDetail extends React.Component {
-  state = {
-    book: {}
-  };
+    state = {
+        book: {}
+    };
 
-  componentDidMount() {
-    const bookID = this.props.match.params.id;
-    axios.get("http://127.0.0.1:8000/books/api/${bookID}").then(res => {
-      this.setState({
-        book: res.data
-      });
-      console.log(res.data);
-    });
-  }
+    componentDidMount() {
+        const bookID = this.props.match.params.bookID;
+        axios.get(`http://127.0.0.1:8000/books/api/${bookID}`).then(res => {
+            this.setState({
+                book: res.data
+            });
+            console.log(res.data);
+        });
+    }
 
-  render() {
-    return (
-      <Card title={this.state.book.title}>
-        <p>{this.state.book.description}</p>
-      </Card>
-    );
-  }
+    render() {
+        return (
+            <Card title={this.state.book.title}>
+                <p>{this.state.book.description}</p>
+            </Card>
+        );
+    }
 }
 
 export default BookDetail;
