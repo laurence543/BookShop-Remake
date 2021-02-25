3d32e7a979a5c888e52653c1099ed36e5651587c
Ivan Holikov <laurence4312@gmail.com>
2021-02-25 15:32:48 +0200

    Replaced BookList with BaseRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import 'antd/dist/antd.css';
-
+import {BrowserRouter as Router} from "react-router-dom";
 import CustomLayout from './containers/Layout';
-import BookList from "./containers/BookListView";
+import BaseRouter from "./routes";
 
 class App extends Component {
-
-  render() {
-    return (
-      <div>
-        <CustomLayout>
-            <BookList/>
-        </CustomLayout>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Router>
+                    <CustomLayout>
+                        <BaseRouter/>
+                    </CustomLayout>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
