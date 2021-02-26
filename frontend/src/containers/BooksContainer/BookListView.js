e2e8b332a12f44e7cccfaa4aa76a155832dd19bc
Ivan Holikov <laurence4312@gmail.com>
2021-02-26 11:36:34 +0200

    Relocated BookList&BookDetail Views

---
+++ frontend/src/containers/BooksContainer/BookListView.js
@@ -0,0 +1,29 @@
+import React from "react";
+import axios from "axios";
+import Book from "../../components/Book";
+import {Card} from "antd";
+
+class BookList extends React.Component {
+    state = {
+        books: {}
+    };
+
+    componentDidMount() {
+        axios.get(`http://127.0.0.1:8000/books/api/`).then(res => {
+            this.setState({
+                books: res.data
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Book data={this.state.books}/> <br/>
+                <h2> Create a book </h2>
+            </div>
+        );
+    }
+}
+
+export default BookList;
