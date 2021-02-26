72a132852cd7c80f90a70fbd809f0fa7f257347b
Ivan Holikov <laurence4312@gmail.com>
2021-02-26 11:42:02 +0200

    Updated routes.js with Common Pages

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
-import BookList from "./containers/BookListView";
-import BookDetail from "./containers/BookDetailView";
+import BookList from "./containers/BooksContainer/BookListView";
+import BookDetail from "./containers/BooksContainer/BookDetailView";
+import Home from "./containers/CommonPagesContainer/Home";
+import About from "./containers/CommonPagesContainer/About";
+import Contacts from "./containers/CommonPagesContainer/Contacts";
 
 const BaseRouter = () => (
     <div>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/contacts" component={Contacts}/>
         <Route exact path="/books" component={BookList}/>
         <Route exact path="/books/:bookID/" component={BookDetail}/>
     </div>
