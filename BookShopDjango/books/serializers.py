b8bfd0b4e2ffd290925f97cffccb06cd14143246
Ivan Holikov <laurence4312@gmail.com>
2021-02-26 20:55:04 +0200

    Added update method

--- BookShopDjango/books/serializers.py
+++ BookShopDjango/books/serializers.py
@@ -46,3 +46,17 @@ class BookListSerializer(serializers.ModelSerializer):
         validated_data['publisher'] = chosen_publisher
         Book.objects.create(**validated_data)
         return validated_data
+
+    def update(self, instance, validated_data):
+        chosen_publisher = Publisher.objects.get(publisher=validated_data['publisher']['publisher'])
+        validated_data['publisher'] = chosen_publisher
+        instance.title = validated_data.get('title', instance.title)
+        instance.author = validated_data.get('author', instance.author)
+        instance.description = validated_data.get('description', instance.description)
+        instance.publish_year = validated_data.get('publish_year', instance.publish_year)
+        instance.stock = validated_data.get('stock', instance.stock)
+        instance.price = validated_data.get('price', instance.price)
+        instance.publisher = validated_data.get('publisher', instance.publisher)
+        instance.image = validated_data.get('image', instance.image)
+        instance.save()
+        return instance
