d8d0d29c5409e3517a85c5df1302a81d6bf629d3
Ivan Holikov <laurence4312@gmail.com>
2021-02-26 12:12:37 +0200

    Relocated Book Component files to single folder

--- frontend/src/containers/BooksContainer/BookDetailView.js
+++ frontend/src/containers/BooksContainer/BookDetailView.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import Book from "../../components/Book";
+import Book from "../../components/Book/Book";
 import {Card} from "antd";
 
 class BookDetail extends React.Component {
--- frontend/src/containers/BooksContainer/BookListView.js
+++ frontend/src/containers/BooksContainer/BookListView.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import Book from "../../components/Book";
+import Book from "../../components/Book/Book";
 
 class BookList extends React.Component {
     state = {
