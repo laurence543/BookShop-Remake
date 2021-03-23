ace0203fbc74a1608ece4a24c79059676600b778
Ivan Holikov <laurence4312@gmail.com>
2021-03-23 19:56:48 +0200

    Added&Registered new User model

--- BookShopDjango/access/models.py
+++ BookShopDjango/access/models.py
@@ -1,18 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser  # , User
 from books.models import Book
 
 
 # Create your models here.
-class User(models.Model):
-    login = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    reg_date = models.DateTimeField()
-    status = models.CharField(max_length=50)
+class User(AbstractUser):
+    GENDERS = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    )
+
+    gender = models.CharField(max_length=1, choices=GENDERS, null=True)
+    birth_date = models.DateField(null=True)
+    tel = models.BigIntegerField(null=True)
+    location = models.CharField(max_length=150, null=True)
+    status = models.CharField(max_length=50, default="customer")
+    modified_at = models.DateTimeField(auto_now=True)
 
 
 class Order(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # User
     order_date = models.DateTimeField(auto_now_add=True)
 
 
