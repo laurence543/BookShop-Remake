99d704192bf2302a9a3ef620ca8a408b1a2d07a5
Ivan Holikov <laurence4312@gmail.com>
2021-03-23 20:45:28 +0200

    Added personal info

--- frontend/src/components/UserInfo/UserInfo.js
+++ frontend/src/components/UserInfo/UserInfo.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import {List, Avatar, Space, Descriptions} from 'antd';
+import moment from "moment";
 
 const UserInfo = (props) => {
     console.log(props)
     return (
 
         <Descriptions title="User Info">
-            <Descriptions.Item label="UserName">{props.data.username}</Descriptions.Item>
-            <Descriptions.Item label="Telephone">{props.data.tel}</Descriptions.Item>
+            <Descriptions.Item label="User Name">{props.data.username}</Descriptions.Item>
+            <Descriptions.Item label="Telephone">+380{props.data.tel}</Descriptions.Item>
             <Descriptions.Item label="Email">{props.data.email}</Descriptions.Item>
-            <Descriptions.Item label="Live">Hangzhou, Zhejiang</Descriptions.Item>
-            <Descriptions.Item label="Remark">empty</Descriptions.Item>
-            <Descriptions.Item label="Address">
-                No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
+            <Descriptions.Item label="First Name">{props.data.first_name}</Descriptions.Item>
+            <Descriptions.Item label="Date of Birth">
+                {moment(props.data.birth_date).format('DD/MM/YYYY')}
             </Descriptions.Item>
+            <Descriptions.Item label="Gender">{props.data.gender}</Descriptions.Item>
+            <Descriptions.Item label="Last Name">{props.data.last_name}</Descriptions.Item>
+            <Descriptions.Item label="Location">{props.data.location}</Descriptions.Item>
         </Descriptions>
     )
 }
