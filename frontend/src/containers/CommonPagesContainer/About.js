68bc953321d07517dd21c98300d8bfcaa6979890
Ivan <laurence4312@gmail.com>
2021-05-19 10:23:32 +0300

    Added 'lorem ipsum dolor'

--- frontend/src/containers/CommonPagesContainer/About.js
+++ frontend/src/containers/CommonPagesContainer/About.js
@@ -12,6 +12,51 @@ class About extends React.Component {
                     <br/>
                     Ми здатні здійснити Вашу мрію!
                 </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et imperdiet mauris, at pharetra
+                    metus. Nulla consequat maximus leo vel blandit. Nam porttitor nunc quis turpis placerat finibus.
+                    Ut euismod turpis sed lacinia porttitor. Vivamus id dui accumsan, facilisis dolor vel, ullamcorper
+                    augue. Pellentesque pellentesque diam ac vestibulum condimentum. Nulla porttitor accumsan porttitor.
+                    Duis ac vulputate enim, sit amet gravida ante. Nullam eget leo a velit semper pellentesque nec
+                    ut eros.
+                </p>
+                <p>
+                    Sed ut enim sit amet sapien tristique suscipit vel ac arcu. Nullam ut erat quis dolor
+                    lobortis iaculis. In a lectus mollis, consectetur dui at, luctus eros.
+                    Cras quis risus et metus malesuada laoreet eget at ex. Nunc non posuere nibh.
+                    Nullam lorem dolor, vulputate ac ornare vitae, bibendum at nunc. Cras nec purus et
+                    tellus mattis lacinia. Nam semper lacus id lorem scelerisque, at tempor lacus vulputate.
+                    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                    Ut posuere orci aliquet nulla ultrices, id luctus turpis consectetur.
+                </p>
+                <p>
+                    Duis vel sem eros. Praesent eget velit vitae lacus bibendum volutpat. Suspendisse id euismod arcu.
+                    Nullam condimentum odio diam, sit amet mattis purus ultrices in. Nullam non suscipit elit.
+                    Fusce volutpat bibendum massa quis pulvinar. In hac habitasse platea dictumst.
+                    Aenean gravida vitae lacus aliquam elementum. Suspendisse luctus ac magna ac pretium.
+                    Duis magna libero, faucibus a varius ut, efficitur ac quam.
+                </p>
+                <p>
+                    Pellentesque id tellus vel justo dictum ultricies ut a ligula. Phasellus maximus tristique felis.
+                    Maecenas augue elit, elementum et commodo eu, congue non sem. Vivamus tempus ante dolor, sed
+                    auctor sapien sagittis vel. Duis sed tellus aliquam, bibendum elit a, gravida nulla. Ut a sem
+                    elit. Nam porttitor pellentesque lectus, vitae aliquet lorem. Sed efficitur augue odio, in
+                    scelerisque sem varius in. Morbi euismod, metus sit amet finibus molestie, justo erat consequat
+                    metus, vel varius ligula lacus vel magna. Sed pellentesque aliquam pellentesque. Aenean sed elit
+                    orci.
+                </p>
+                <p>
+                    Nullam convallis lectus erat, a ornare metus tincidunt sit amet.
+                    Pellentesque vitae mi felis. In sed leo rutrum, egestas risus a, aliquet nisl.
+                    Quisque semper ligula nec posuere venenatis. Sed vulputate quam vitae iaculis
+                    fringilla. In at lacus sed tortor malesuada luctus non nec libero. Maecenas non
+                    libero et ex venenatis tristique. Nam bibendum mauris in semper porta. Proin
+                    rutrum ac lorem vel feugiat. Phasellus pulvinar felis odio, et venenatis dui
+                    maximus quis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+                    posuere cubilia curae; Maecenas interdum in sapien non commodo. Aliquam turpis
+                    tellus, fringilla vitae condimentum et, interdum a lorem. Sed et eleifend enim,
+                    sed mollis dui. Nam imperdiet dui a quam pellentesque, ac aliquam est porta.
+                </p>
             </div>
         );
     }
