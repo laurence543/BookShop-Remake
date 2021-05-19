c2a8040550ff289143fe11d56f5eeace58ff2b61
Ivan <laurence4312@gmail.com>
2021-05-19 11:56:11 +0300

    Made minor text changes

--- frontend/src/containers/CommonPagesContainer/Home.js
+++ frontend/src/containers/CommonPagesContainer/Home.js
@@ -15,6 +15,16 @@ class Home extends React.Component {
         return (
             <div>
                 <h1>Головна сторінка</h1>
+                <p>
+                    <h3>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et imperdiet mauris, at pharetra
+                        metus. Nulla consequat maximus leo vel blandit. Nam porttitor nunc quis turpis placerat finibus.
+                        Ut euismod turpis sed lacinia porttitor. Vivamus id dui accumsan, facilisis dolor vel, ullamcorper
+                        augue. Pellentesque pellentesque diam ac vestibulum condimentum. Nulla porttitor accumsan porttitor.
+                        Duis ac vulputate enim, sit amet gravida ante. Nullam eget leo a velit semper pellentesque nec
+                        ut eros.
+                    </h3>
+                </p>
                 <Carousel autoplay>
                     <div>
                         <h3 style={contentStyle}>Безкоштовна доставка при замовленні від 700 грн.</h3>
@@ -25,6 +35,7 @@ class Home extends React.Component {
                         </h3>
                     </div>
                 </Carousel>
+
             </div>
         );
     }
