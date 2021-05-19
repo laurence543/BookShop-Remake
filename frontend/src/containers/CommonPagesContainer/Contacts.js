1ccb5cbbd734befc0394f9b4ed67d14fee343be0
Ivan <laurence4312@gmail.com>
2021-05-19 10:05:03 +0300

    Added common fake info

--- frontend/src/containers/CommonPagesContainer/Contacts.js
+++ frontend/src/containers/CommonPagesContainer/Contacts.js
@@ -1,11 +1,31 @@
 import React from "react";
+import {InstagramFilled, FacebookFilled, TwitterCircleFilled} from '@ant-design/icons';
 
 class Contacts extends React.Component {
 
     render() {
         return (
             <div>
-                <p>Сторінка Контакти</p>
+                <h1>Контакти</h1>
+                <div>
+                    <h2>Телефони</h2>
+                    <h3>+380 96 123 45 67</h3>
+                    <h3>+380 73 123 45 67</h3>
+                    <h3>+380 50 123 45 67</h3>
+                </div>
+                <br/>
+                <div>
+                    <h2>Пошта</h2>
+                    <h3>bookshop@gmail.com</h3>
+                </div>
+                <br/>
+                <div>
+                    <h2>Ми у соціальних мережах</h2>
+                    <h3><InstagramFilled /> <a href="https://www.instagram.com/" target="_blank">bookshop</a></h3>
+                    <h3><FacebookFilled /> <a href="https://www.facebook.com/" target="_blank">bookshop</a></h3>
+                    <h3><TwitterCircleFilled /> <a href="https://twitter.com/" target="_blank">bookshop</a></h3>
+
+                </div>
             </div>
         );
     }
--- frontend/src/containers/CommonPagesContainer/Home.js
+++ frontend/src/containers/CommonPagesContainer/Home.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { Carousel } from 'antd';
+
+const contentStyle = {
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+};
 
 class Home extends React.Component {
 
     render() {
         return (
             <div>
-                <p>Головна сторінка</p>
+                <h1>Головна сторінка</h1>
+                <Carousel autoplay>
+                    <div>
+                        <h3 style={contentStyle}>Безкоштовна доставка при замовленні від 700 грн.</h3>
+                    </div>
+                    <div>
+                        <h3 style={contentStyle}>
+                            Іменинникам знижка 20% (при наявності паспорту)
+                        </h3>
+                    </div>
+                </Carousel>
             </div>
         );
     }
